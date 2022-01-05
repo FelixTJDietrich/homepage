1822968471f43cd955c73988b344af209a1e3c9e
Felix Dietrich <felixtj.dietrich@tum.de>
2022-01-05 11:06:30 +0100

    Remove recent posts from sidebar and add read more

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Card, CardTitle, CardBody, CardText } from "reactstrap";
-import { GatsbyImage, StaticImage, getImage } from "gatsby-plugin-image"
-import { graphql, Link, StaticQuery } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby";
 
-import author from "../util/author";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
   faTwitter, 
@@ -16,97 +15,53 @@ import {
   faEnvelopeSquare,
 } from '@fortawesome/free-solid-svg-icons'
 
+import author from "../util/author";
+
 const Sidebar = () => (
   <div>
     <Card>
       <StaticImage className="card-image-top" src="../images/author.jpg"/>
       <CardBody>
         <CardTitle className="text-center text-uppercase mb-3">{author.name}</CardTitle>
-        <CardText>{author.bio}</CardText>
-        
+        <CardText>
+          {author.bio}
+          <br/>
+          <Link to={"/about"} className="read-more">
+            Read more →
+          </Link>
+        </CardText>  
         <div className="author-social-links text-left">
           <ul>
             <li style={{ marginBottom: "5px" }}>
-              <a href={author.location.url} target="_blank" rel="noopener noreferrer" className="location">
+              <Link href={author.location.url} target="_blank" rel="noopener noreferrer" className="location">
                 <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" fixedWidth/>{author.location.title}
-              </a>
+              </Link>
             </li>
             <li>
-              <a href={"mailto:" + author.email} target="_blank" rel="noopener noreferrer" className="email">
+              <Link href={"mailto:" + author.email} target="_blank" rel="noopener noreferrer" className="email">
                 <FontAwesomeIcon icon={faEnvelopeSquare} size="lg" fixedWidth/>Email
-              </a>
+              </Link>
             </li>
             <li>
-              <a href={"https://www.twitter.com/" + author.handles.twitter} target="_blank" rel="noopener noreferrer" className="twitter">
+              <Link href={"https://www.twitter.com/" + author.handles.twitter} target="_blank" rel="noopener noreferrer" className="twitter">
                 <FontAwesomeIcon icon={faTwitter} size="lg" fixedWidth/>Twitter
-              </a>
+              </Link>
             </li>
             <li>
-              <a href={"https://www.instagram.com/" + author.handles.instagram} target="_blank" rel="noopener noreferrer" className="instagram">
+              <Link href={"https://www.instagram.com/" + author.handles.instagram} target="_blank" rel="noopener noreferrer" className="instagram">
                 <FontAwesomeIcon icon={faInstagram} size="lg" fixedWidth/>Instagram
-              </a>
+              </Link>
             </li>
             <li>
-              <a href={"https://www.github.com/" + author.handles.github} target="_blank" rel="noopener noreferrer" className="github">
+              <Link href={"https://www.github.com/" + author.handles.github} target="_blank" rel="noopener noreferrer" className="github">
                 <FontAwesomeIcon icon={faGithub} size="lg" fixedWidth/>GitHub
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
       </CardBody>
     </Card>
-    <Card>
-      <CardBody>
-        <CardTitle className="text-center text-uppercase mb-3">
-          Recent Posts
-        </CardTitle>
-        <StaticQuery query={sidebardQuery} render={(data) => (
-          <div>
-            {data.allMarkdownRemark.edges.map(({ node }) => (
-              <Card key={node.id}>
-                <Link to={`/${node.fields.slug}`}>
-                  <GatsbyImage className="card-image-top" image={getImage(node.frontmatter.image)}/>
-                </Link>
-                <CardBody>
-                  <CardTitle>
-                    <Link to={`/${node.fields.slug}`}>
-                      {node.frontmatter.title}
-                    </Link>
-                  </CardTitle>
-                </CardBody>
-              </Card>
-            ))}
-          </div>
-        )} />
-      </CardBody>
-    </Card>
   </div>
 )
-
-const sidebardQuery = graphql`
-  query sidebarQuery {
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      limit: 3
-    ) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            image {
-              childImageSharp {
-                gatsbyImageData(width: 300)
-              }
-            }
-          }
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  }
-`
 
 export default Sidebar
